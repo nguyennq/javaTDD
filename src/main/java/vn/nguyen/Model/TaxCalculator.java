7f02439614d9ab39cf2897cfe1674bd8a755150f
nals <nguyen_nq@hotmail.com>
2018-02-07 16:20:58 +0700

    Bo sung UserImpl and Test Bo sung TaxCalculator with TDD step by step

--- src/main/java/vn/nguyen/Model/TaxCalculator.java
+++ src/main/java/vn/nguyen/Model/TaxCalculator.java
@@ -5,6 +5,9 @@ package vn.nguyen.Model;
  */
 public class TaxCalculator {
     public double calculate(double taxableIncome) {
-        return 400000.00;
+        if(taxableIncome > 500000){
+            return 50000 + ((taxableIncome-500000)/5);
+        }
+        return (taxableIncome/10);
     }
 }
--- src/test/java/vn/nguyen/Model/TaxCalculatorTest.java
+++ src/test/java/vn/nguyen/Model/TaxCalculatorTest.java
@@ -12,8 +12,27 @@ public class TaxCalculatorTest {
     @Test
     public void when_income_less_than_5Lacs_then_deducts_10_percent_tax() throws Exception {
         TaxCalculator taxCalculator = new TaxCalculator();
-        final double payableTax = taxCalculator.calculate(400000.00);
-        assertTrue(400000.00 == payableTax);
+        double payableTax = taxCalculator.calculate(400000.00);
+        assertTrue(40000 == payableTax);
+        //Check boundary values 0 and 500,000.
+        payableTax = taxCalculator.calculate(0);
+        assertTrue(0 == payableTax);
 
+        payableTax = taxCalculator.calculate(500000.00);
+        assertTrue(50000 == payableTax);
+
+
+    }
+
+    @Test
+    public void when_income_between_5lacs_and_10lacs_then_deducts_fifty_thousand_plus_20_percent_above_5lacs() throws Exception {
+        TaxCalculator taxCalculator = new TaxCalculator();
+        double payableTax = taxCalculator.calculate(800000.00);
+        double expectedTaxForFirstFiveHundredThousand = 50000;
+        double expectedTaxForReminder = 60000;
+        double expectedTotalTax =
+                expectedTaxForFirstFiveHundredThousand +
+                        expectedTaxForReminder;
+        assertTrue(expectedTotalTax == payableTax);
     }
 }
