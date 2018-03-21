c9409342faaac71f46f9cc53a22f79242bb7be04
nals <nguyen_nq@hotmail.com>
2018-03-21 09:27:37 +0700

    StringHelper with swapChar with step by step

--- src/main/java/vn/nguyen/Model/StringHelper.java
+++ src/main/java/vn/nguyen/Model/StringHelper.java
@@ -5,9 +5,15 @@ package vn.nguyen.Model;
  */
 public class StringHelper {
     public String swapLast2Chars(String text) {
-//      swap  BA = AB
-        char firstChar = text.charAt(0);
-        char secondChar = text.charAt(1);
-        return "" + secondChar + firstChar;
+        //swap  BA = AB
+//        char firstChar = text.charAt(0);
+//        char secondChar = text.charAt(1);
+//        return "" + secondChar + firstChar;
+        //swap ABDC = ABCD
+        int lengthOfText = text.length();
+        String textMinusLast2Chars = text.substring(0,lengthOfText-2);
+        char secondLastChar = text.charAt(lengthOfText - 2);
+        char lastChar = text.charAt(lengthOfText - 1);
+        return textMinusLast2Chars + lastChar + secondLastChar;
     }
 }
--- src/test/java/vn/nguyen/Model/StringHelperTest.java
+++ src/test/java/vn/nguyen/Model/StringHelperTest.java
@@ -27,7 +27,21 @@ public class StringHelperTest {
     @Test
     public void testswapLast2CharsWith4Char() throws Exception {
         StringHelper stringHelper = new StringHelper();
-        String actual = "ABCD";
+        String actual = "ABDC";
         assertThat(actual,is(stringHelper.swapLast2Chars("ABCD")));
+    }
+
+    @Test
+    public void testswapLast2CharsWith10Char() throws Exception {
+        StringHelper stringHelper = new StringHelper();
+        String actual = "ABCDEFGHJI";
+        assertThat(actual,is(stringHelper.swapLast2Chars("ABCDEFGHIJ")));
+    }
+
+    @Test
+    public void testswapLast2CharsWith1Char() throws Exception {
+        StringHelper stringHelper = new StringHelper();
+        String actual = "A";
+        assertThat(actual,is(stringHelper.swapLast2Chars("A")));
     }
 }
