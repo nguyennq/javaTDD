90e5a396720e7fbc61cddcc101e6ced27ca8ae38
nals <nguyen_nq@hotmail.com>
2018-03-21 14:24:21 +0700

    FizzBuzz refactor first step final

--- src/main/java/vn/nguyen/Model/FizzBuzz.java
+++ src/main/java/vn/nguyen/Model/FizzBuzz.java
@@ -5,6 +5,16 @@ package vn.nguyen.Model;
  */
 public class FizzBuzz {
     public String numberFizzBuzz(int number) {
+        if ((number % 3 == 0) & (number % 5 == 0)){
+            return "FizzBuzz";
+        }
+        else if (number % 3 == 0){
+            return "Fizz";
+        }
+        else if (number % 5 == 0){
+            return "Buzz";
+        }
+
         return String.valueOf(number);
     }
 }
--- src/test/java/vn/nguyen/Model/FizzBuzzTest.java
+++ src/test/java/vn/nguyen/Model/FizzBuzzTest.java
@@ -11,15 +11,48 @@ import static org.junit.Assert.*;
  * Created by nals on 3/21/18.
  */
 public class FizzBuzzTest {
+    private FizzBuzz fizzBuzz;
     @Before
     public void setUp() throws Exception {
         MockitoAnnotations.initMocks(this);
+        fizzBuzz = new FizzBuzz();
     }
 
     @Test
-    public void testNumberShouldReturn1WhenInputNumnerIs1() throws Exception {
-        FizzBuzz fizzBuzz = new FizzBuzz();
+    public void testnumberFizzBuzzShouldReturn1WhenInputNumnerIs1() throws Exception {
         String actual = "1";
         assertThat(actual,is(fizzBuzz.numberFizzBuzz(1)));
     }
+
+    @Test
+    public void testnumberFizzBuzzShouldReturnFizzWhenInputNumberIs3() throws Exception {
+        String actual = "Fizz";
+        assertThat(actual,is(fizzBuzz.numberFizzBuzz(3)));
+    }
+
+    @Test
+    public void testnumberFizzBuzzShouldReturnFizzWhenInputNumberDivisibleBy3() throws Exception {
+        String actual = "Fizz";
+        assertThat(actual,is(fizzBuzz.numberFizzBuzz(6)));
+    }
+
+    @Test
+    public void testnumberFizzBuzzShouldReturnBuzzWhenInputNumberIs5() throws Exception {
+        String actual = "Buzz";
+        assertThat(actual,is(fizzBuzz.numberFizzBuzz(5)));
+    }
+
+    @Test
+    public void testnumberFizzBuzzShouldReturnBuzzWhenInputNumberDivisibleBy5() throws Exception {
+        String actual = "Buzz";
+        assertThat(actual,is(fizzBuzz.numberFizzBuzz(10)));
+    }
+
+    @Test
+    public void testnumberFizzBuzzShouldReturnFizzBuzzWhenInputNumberDivisibleBy3and5() throws Exception {
+        String actual = "FizzBuzz";
+        assertThat(actual,is(fizzBuzz.numberFizzBuzz(15)));
+    }
+
+
 }
